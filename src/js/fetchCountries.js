0c73844b2e5f42fc8d2c68e961ffefac63f68106
grey4uk <grey4uk14@gmail.com>
2020-02-19 12:17:04 +0200

    add results to localStorage in hw12

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,14 +2,41 @@ import '../../node_modules/pnotify/dist/PNotifyBrightTheme.css';
 import PNotify from '../../node_modules/pnotify/dist/es/PNotify.js';
 import PNotifyButtons from '../../node_modules/pnotify/dist/es/PNotifyButtons.js';
 
-export default function fetchCountries(searchQuery, neededCountry) {
-  return fetch(searchQuery)
-    .then(data => data.json())
-    .then(data => {
-      return neededCountry(data);
-    })
-    .catch(error => {
-      PNotify.error({ text: 'Error of country name!' });
-      }
-    );
+// export default function fetchCountries(searchQuery, neededCountry) {
+//   return fetch(searchQuery)
+//     .then(data => data.json())
+//     .then(data => {
+//       return neededCountry(data);
+//     })
+//     .catch(error => {
+//       PNotify.error({ text: 'Error of country name!' });
+//       }
+//     );
+// }
+let storage = {};
+const STORAGE = "ourApp";
+export default function fetchCountries(searchQuery, neededCountry){
+  
+  if (localStorage.getItem(STORAGE)){
+    storage = JSON.parse(localStorage.getItem(STORAGE));
+  }else{
+    localStorage.setItem(STORAGE, JSON.stringify(storage));
+  }
+  
+    if (storage[searchQuery]){
+      neededCountry(storage[searchQuery]);
+      return;
+    }
+    fetch(searchQuery).then(response=>response.json()).
+    then(data=>{
+      
+      storage[searchQuery] = data;
+      localStorage.setItem(STORAGE, JSON.stringify(storage));
+      neededCountry(data);}
+    
+    ).
+    catch(error=>{
+      PNotify.error(error);
+      throw new Error(error);
+    });
 }
